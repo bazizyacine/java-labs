cea6bd388719b80b8d1dd780a0380e4cea0926ac
bazizyacine <58179990+bazizyacine@users.noreply.github.com>
2019-12-17 12:14:37 +0100

    test

---
+++ A.java
@@ -0,0 +1,14 @@
+package edu.A;
+
+public class A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new edu.B.B();
+	}
+	
+	public A() {
+		System.out.println("Hello package A!");
+	}
+
+}
---
+++ B.java
@@ -0,0 +1,14 @@
+package edu.B;
+
+public class B {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new edu.A.A();
+	}
+
+	public B() {
+		System.out.println("Hello package B!");
+	}
+
+}
