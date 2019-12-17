eb0f55513bb297b4fb708d4b0a0dbb13365a8d88
bazizyacine <58179990+bazizyacine@users.noreply.github.com>
2019-12-17 09:29:21 +0100

    test

--- lab4.java
+++ lab4.java
@@ -1,9 +1,9 @@
 public class lab4 {
 	public static void main(String[] args) {
-		Point p1 = new Point ();
-		Point p2 = new Point (1);
-		Point p3 = new Point (1, 2);
-		Point p4 = new Point (p3);
+		Point p1 = new Point();
+		Point p2 = new Point(1);
+		Point p3 = new Point(1, 2);
+		Point p4 = new Point(p3);
 		
 		System.out.printf("p1.abs = %d\np1.abs = %d"
 							+ "\np2.abs = %d\np2.abs = %d"
