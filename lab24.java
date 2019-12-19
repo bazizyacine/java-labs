cdfdfb36e79170160b42dfb435fc502d2cf34dc7
bazizyacine <58179990+bazizyacine@users.noreply.github.com>
2019-12-19 15:59:59 +0100

    test

---
+++ lab24.java
@@ -0,0 +1,26 @@
+
+public class lab24 {
+	public static void main(String[] args) {
+		int t[][]={{1, 2, 3},{11, 12},{21, 22, 23, 24}};
+		System.out.println("====> t avant raz : ");
+		Util.affiche(t);
+		System.out.println ("====> t apres raz : ");
+		Util.raz(t);
+		Util.affiche(t);
+	}
+}
+
+class Util { 
+	 public static void raz(int Mat[][]) { 
+		 for (int i = 0; i < Mat.length; i++) 
+			 for (int j = 0; j < Mat[i].length; j++) 
+				 Mat[i][j] = 0;
+	 } 
+	 
+	 public static void affiche(int Mat[][]) { 
+		 for (int i = 0; i < Mat.length; i++) 
+			 for (int j = 0; j < Mat[i].length; j++) 
+				 System.out.printf("Mat[%d][%d] = %d\n", i, j, Mat[i][j]); 
+	 }
+ }
+ 
