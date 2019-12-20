9629cdfe50ff4e25f344fcf47c13e0da0c9f7321
bazizyacine <58179990+bazizyacine@users.noreply.github.com>
2019-12-20 14:22:44 +0100

    test

--- lab29.java
+++ lab29.java
@@ -1,27 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class lab29 {
 	public static void main(String[] args) {	
-		Couple<Integer> ci = new Couple<Integer>(3, 5);
-		System.out.println(ci);
-		Couple<Double> cd = new Couple<Double>(2.0, 12.0);
-		System.out.println(cd);
-		Double p = cd.getPremier();
-		System.out.println("1er élément du couple = " + p);
+		List<Point> points = new ArrayList<Point>();
+		
+		Point p = new Point(3,3);
+		points.add(p);
+		points.add(new Point(1,1));
+		points.add(new Point(2,2));
+		
+		//Parcours de la liste
+		for(Point point : points)
+			System.out.println(point.getX());
+		
+		//Autres manipulations
+		System.out.println("Taille : " + points.size());
+		System.out.println("Deuxième point : " + points.get(1));
+		System.out.println("Index de p : " + points.indexOf(p));
+		points.remove(1);
+		
+		Object[] t = points.toArray();
+		
+		for(Object o : t)
+			System.out.print(((Point) o).getX());
 	}	
 }
 
-class Couple <T> {
-	private T x, y; // les deux éléments du couple
-	public Couple(T premier, T second) {
-		x = premier;
-		y = second;
-	}
-	public String toString()
-	{
-		// x et y convertis automatiquement par toString
-		return ("Valeur 1 :" + x + "– Valeur 2 :" + y);
-	}
-	T getPremier() {
+class Point {
+	private int x, y;
+
+	public int getX() {
 		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
 	}
 }
