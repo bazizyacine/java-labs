fc90ec0da2315337230681a1a339a7aa31bfbd2e
bazizyacine <58179990+bazizyacine@users.noreply.github.com>
2019-12-20 14:07:57 +0100

    test

---
+++ lab28.java
@@ -0,0 +1,27 @@
+
+public class lab28 {
+	public static void main(String[] args) {	
+		Couple<Integer> ci = new Couple<Integer>(3, 5);
+		System.out.println(ci);
+		Couple<Double> cd = new Couple<Double>(2.0, 12.0);
+		System.out.println(cd);
+		Double p = cd.getPremier();
+		System.out.println("1er élément du couple = " + p);
+	}	
+}
+
+class Couple <T> {
+	private T x, y; // les deux éléments du couple
+	public Couple(T premier, T second) {
+		x = premier;
+		y = second;
+	}
+	public String toString()
+	{
+		// x et y convertis automatiquement par toString
+		return ("Valeur 1 :" + x + "– Valeur 2 :" + y);
+	}
+	T getPremier() {
+		return x;
+	}
+}
